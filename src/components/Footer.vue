e9ae6cc3da35af6645ff6e87208641ff0245fe50
augustopereiratx <augustopt03@gmail.com>
2024-02-01 10:37:12 -0500

    small changes

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,8 +2,8 @@
     <footer id="footer">
         <div class="row">
             <div class="col-md-6 text-justify">
-                <p>This website is licensed under the GNU <a href="https://www.gnu.org/licenses/gpl-3.0.en.html">GPLv3</a>
-                    license. Source code can be found <a href="https://github.com/augustotx/augustotx.github.io">here</a>.
+                <p>This website is licensed under the GNU <a target="_blank" href="https://www.gnu.org/licenses/gpl-3.0.en.html">GPLv3</a>
+                    license. Source code can be found <a target="_blank" href="https://github.com/augustotx/augustotx.github.io">here</a>.
                 </p>
             </div>
             <div class="col-md text-center">
